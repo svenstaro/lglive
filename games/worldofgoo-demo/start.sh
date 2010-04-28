cbb59018fb2407abb526e53aa4536c3638cdbc0f
Sven-Hendrik Haase <sh@lutzhaase.com>
2010-04-29 00:09:39 +0200

    Added game: osmos and improved launchner script

--- games/worldofgoo-demo/start.sh
+++ games/worldofgoo-demo/start.sh
@@ -1,2 +1,2 @@
 cd /opt/games/worldofgoo-demo
-xterm -e 'dialog --msgbox "You are about to see the license for this game.\nYou may not play the game if you do not agree to the license.\nWhen you are done reading, please press \"Q\" and decide whether you agree to the license." 10 60 && lynx addons/eula.txt && dialog --defaultno --yesno "Do you accept this license?" 8 50 && worldofgoo'
+zenity --info --text "You are about to see the license for this game.\nYou may not play the game if you do not agree to the license.\nWhen you are done reading, please press capital \"Q\" and decide whether you agree to the license." 10 60 && lynx addons/eula.txt && zenity --question --text "Do you accept this license?" 8 50 && worldofgoo'
