ced8fdede84bef118779e1c2fa3fe9ee92ada795
Sven-Hendrik Haase <sh@lutzhaase.com>
2010-05-12 02:25:20 +0200

    Fixed urban-terror init script

--- games/urbanterror/init.sh
+++ games/urbanterror/init.sh
@@ -1,3 +1,3 @@
 echo 'seta r_mode "-1"
 seta r_customwidth "1280"
-seta r_customheight "1024"' > ~/.urbanterror/q3ut4/autoexec.cfg
+seta r_customheight "1024"' > /home/gamer/.urbanterror/q3ut4/autoexec.cfg
