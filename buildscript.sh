f0f1387fb27434b16c632a28a82d0aafdb0af5c8
Sven-Hendrik Haase <sh@lutzhaase.com>
2011-03-13 21:10:37 +0100

    Copy poweroff.com

--- buildscript.sh
+++ buildscript.sh
@@ -211,6 +211,7 @@ bootloader ()
     cp "${WORKDIR}/root-image/usr/lib/syslinux/pxelinux.0" "${WORKDIR}/iso/boot/syslinux/" || return 1
     cp "${WORKDIR}/root-image/usr/lib/syslinux/gpxelinux.0" "${WORKDIR}/iso/boot/syslinux/" || return 1
     cp "${WORKDIR}/root-image/usr/lib/syslinux/"*.c32 "${WORKDIR}/iso/boot/syslinux/" || return 1
+    cp "${WORKDIR}/root-image/usr/lib/syslinux/poweroff.com "${WORKDIR}/iso/boot/syslinux/" || return 1
     #sed "s|archisolabel=[^ ]*|archisolabel=${NAME}-${VER//./}|" -i ${WORKDIR}/iso/boot/pxelinux.cfg/default || return 1
     [ "$?" -ne 0 ] && echo -e "\e[01;31mbootloader Exiting due to error while copying bootloader\e[00m" && exit 1
   fi
