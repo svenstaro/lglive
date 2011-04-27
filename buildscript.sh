76a4eaf4607146dd95055ac723c014a5f42493d9
Sven-Hendrik Haase <sh@lutzhaase.com>
2011-04-27 09:15:29 +0200

    buildscript.sh: Don't clean packages that don't exist

--- buildscript.sh
+++ buildscript.sh
@@ -245,7 +245,7 @@ build ()
   rm -rf "${WORKDIR}"/root-image/usr/src/*
   rm -rf "${WORKDIR}"/root-image/usr/share/doc/*
   rm -rf "${WORKDIR}"/root-image/usr/share/man/*
-  pacman -Rsn --root "${BASEDIR}/${WORKDIR}/root-image/" --dbpath "${BASEDIR}/${WORKDIR}/root-image/var/lib/pacman" --config ${BASEDIR}/pacman.conf --noconfirm man-db man-pages || return 1
+  #pacman -Rsn --root "${BASEDIR}/${WORKDIR}/root-image/" --dbpath "${BASEDIR}/${WORKDIR}/root-image/var/lib/pacman" --config ${BASEDIR}/pacman.conf --noconfirm man-db man-pages || return 1
   #pacman -Q --root "${BASEDIR}/${WORKDIR}/root-image/" --dbpath "${BASEDIR}/${WORKDIR}/root-image/var/lib/pacman" --config ${BASEDIR}/pacman.conf --noconfirm doxygen && pacman -Rsn --root "${BASEDIR}/${WORKDIR}/root-image/" --dbpath "${BASEDIR}/${WORKDIR}/root-image/var/lib/pacman" --config ${BASEDIR}/pacman.conf --noconfirm doxygen
   echo ${TARGET} | grep -q "lite" && edition="lite" || edition="big"
   [ ! ${QUIET} == "y" ] && echo "build: Setting edition to '${edition}'"
