ee314acf9be9e09b81ca1624d700717981b2db61
Sven-Hendrik Haase <sh@lutzhaase.com>
2011-04-29 02:53:52 +0200

    Fixed lgames to run at fullscreen

---
+++ games/lbreakout2/init.sh
@@ -0,0 +1,2 @@
+install -D -m644 /opt/games/lbreakout2/addons/lbreakout2.conf /home/gamer/.lgames/lbreakout2.conf
+chown -R gamer:users /home/gamer/.lgames
---
+++ games/ltris/init.sh
@@ -0,0 +1,2 @@
+install -D -m644 /opt/games/ltris/addons/ltris.conf /home/gamer/.lgames/ltris.conf
+chown -R gamer:users /home/gamer/.lgames
