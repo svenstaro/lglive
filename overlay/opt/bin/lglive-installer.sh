ae06edb3fe76d1416946384c9f4b2bf115f6c1bb
opatut <opatutlol@aol.com>
2011-04-30 17:34:31 +0200

    lglive-installer.sh: Implemented auto-partitioning

--- overlay/opt/bin/lglive-installer.sh
+++ overlay/opt/bin/lglive-installer.sh
@@ -1,85 +1,68 @@
 #!/bin/bash
 
-WELCOME_MESSAGE='Welcome to the <b>live.linuX-gamers Setup</b> wizard. Please follow these simple steps to install a copy of the distro on your machine.
+DEVICE=""
 
-Installing the system will include erasing a whole drive (not only a partition), and copying the required files.
+select_device() {
+    DEVICE=$(zenity --list --title "Select target" --text "Please select the target drive for your live.linuX-gamers installation. <span color='red'>Warning! All data on the drive will be erased.</span>" --column="Drive" $(ls /dev/sd?))
+    if [[ $? != 0 ]]; then
+        exit
+    fi
+    zenity --question --title "Are you sure?" --text "<span color='red' size='x-large'><b>Are you sure?</b> All data on the drive $DEVICE will be deleted.</span>" --cancel-label="Cancel" --ok-label="Yes, erase it."
 
-You may press "Cancel" at any time to quit the wizard.'
-DRIVE_SELECTION_MESSAGE='Please select your target drive.
-
-<span color="red"><b>WARNING!</b> All data on the
-drive will be cleared.</span>'
-DRIVE_ERASE_WARNING='<span size="x-large" color="red">Are you sure you want to erase the whole drive <b>%s</b>?</span>'
-FILE_COPY_COMPLETE='Files successfully copied to target. 
-
-<span color="green"><b>Congratulations!</b> You now have live.linuX-gamers installed on your machine.</span>
-
-Try it out: reboot now!'
-
-IMAGE_SIZE=0
-IMAGE_DRIVE=""
-findout_image_size() {
-    # Find out whether we are on CD, USB or diskless
-    label=`cat /proc/cmdline | grep -io 'archisolabel=[a-zA-Z0-9\-]*' | sed s/'archisolabel='//`
-    IMAGE_DRIVE=`readlink -f /dev/disk/by-label/${label}`
-    drive=`basename ${IMAGE_DRIVE}`
-    blocks=$(cat /sys/block/$drive/size)
-    IMAGE_SIZE=$(( blocks * 512 ))
-}
-
-
-cancelled() {
     if [[ $? != 0 ]]; then
         exit
     fi
 }
 
-TITLE="live.linuX-gamers -- Setup"
+autopart() {
+    # create partition table
+    parted -s $DEVICE mklabel msdos
 
-zenity --question --ok-label="Continue" --cancel-label="Cancel" --title "$TITLE" --text "$WELCOME_MESSAGE"
+    # create boot partition
+    parted -s $DEVICE mkpart primary 1 100
+    parted -s $DEVICE mkpart set 1 boot on
+    mkfs.ext2 -L boot-lglive /dev/sda1
 
-cancelled
+    # create swap partition
+    parted -s $DEVICE mkpart primary 100 612
+    parted -s $DEVICE mkpart set 2 swap on
+    mkswap -L swap-lglive /dev/sda2
+    swapon -L swap-lglive
 
-while true; do
-    TARGET_DRIVE=$(zenity --list --title "$TITLE" --text "$DRIVE_SELECTION_MESSAGE" --column="Drive" `ls /dev/sd?`)
+    # create root partition
+    parted -s $DEVICE mkpart primary 612 100%
+    mkfs.ext4 -L root-lglive /dev/sda3
+}
 
-    cancelled
-    if [[ $TARGET_DRIVE == "" ]]; then
-        zenity --error --title "$TITLE" --text "You have to select a target."
-    else
-        break
-    fi
-done
+copy_files() {
+    # copy boot files
+    mkdir /mnt/boot-lglive/
+    mount -L boot-lglive /mnt/boot-lglive/
+    cp -a /mnt/bootmnt/lglive/boot/* /mnt/boot-lglive/
+    cp -a /mnt/bootmnt/syslinux /mnt/boot-lglive/
 
-DRIVE_ERASE_WARNING=$(echo $DRIVE_ERASE_WARNING | sed 's|%s|'$DRIVE'|g')
+    # copy root files
+    mkdir /mnt/root-lglive/
+    mount -L root-lglive /mnt/root-lglive/
+    cp -a /bin /etc /gamelist_* /home /lib /opt /root /run /sbin /srv /usr /var /mnt/root-lglive/
+    mkdir /mnt/root-lglive/{dev,sys,proc,media,mnt,tmp,boot}
+    chmod 777 /mnt/root-lglive/tmp/
+}
 
-zenity --question --title "$TITLE" --text "$DRIVE_ERASE_WARNING" --cancel-label="Cancel" --ok-label="Yes, erase it"
+install_bootloader() {
+    cat /usr/lib/syslinux/mbr.bin > $DEVICE
 
-cancelled
+    sed -i 's|/boot/lglive|..|g' /mnt/boot-lglive/syslinux/syslinux.cfg
+    extlinux --install /mnt/boot-lglive/syslinux
+    sed -s 's|APPEND initrd|APPEND root=LABEL=root-lglive initrd|g' /mnt/boot-lglive/syslinux/syslinux.cfg
 
-findout_image_size
+    # bake kernel
+    sed -i 's|HOOKS.*|HOOKS="base udev pata scsi sata resume filesystems"|g' /etc/mkinitcpio.conf
+    sed -i 's|kernel26\.img|lglive.img|g' /etc/mkinitcpio.d/kernel26.preset
+    sed -i 's|PRESETS.*|PRESETS=("default")|g' /etc/mkinitcpio.d/kernel26.preset
+    mount --bind /mnt/boot-lglive /boot
+    
+    mkinitcpio -p kernel26
+}
 
-echo "Image size to copy: "$IMAGE_SIZE
-# perform copy operation, watch output in zenity
-# this line was sooo hard work :P
-(dd if=$IMAGE_DRIVE bs=32M | 
-    pv -i 0.1 -n -s $IMAGE_SIZE"k" | 
-    dd of=$TARGET_DRIVE bs=32M) 2>&1 | 
-    zenity --progress --title "$TITLE" --text \
-        "Copying files... (OK only hides this dialog.)" --auto-close --auto-kill --no-cancel
-
-if [[ $? != 0 ]]; then
-    zenity --error --text "Copy operation cancelled. You have to expect your drive to be corrupt and impossible to boot from. Just try it again, it might work!" --title "$TITLE"
-    exit
-fi
-
-zenity --question --ok-label "Reboot" --cancel-label "Don't reboot" \
-    --text "$FILE_COPY_COMPLETE" --title "$TITLE"
-
-cancelled
-
-sudo reboot
-
-# install the bootloader if we want to do some magic instead of simply copying bytes
-
-
+select_device
