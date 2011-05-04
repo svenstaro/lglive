9a166ea2975f73f3fc20638545195a1b68f82f2b
Sven-Hendrik Haase <sh@lutzhaase.com>
2011-05-04 05:45:45 +0200

    Fixing openttd to launch in fullscreen and at proper resolution

---
+++ games/openttd/init.sh
@@ -0,0 +1,5 @@
+install -D -m644 /opt/games/openttd/addons/openttd.cfg /home/gamer/.openttd/openttd.cfg
+xres=$(xrandr -q | grep \* | sed -e "s/^[ \t]*//" -e "s/\([0-9]*x[0-9]*\).*/\1/" | cut -d'x' -f1)
+yres=$(xrandr -q | grep \* | sed -e "s/^[ \t]*//" -e "s/\([0-9]*x[0-9]*\).*/\1/" | cut -d'x' -f2)
+sed -i -e "s/XXXX/$xres/" -e "s/YYYY/$yres/" /home/gamer/.openttd/openttd.cfg
+chown -R gamer:users /home/gamer/.openttd/
