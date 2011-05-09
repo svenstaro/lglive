6581ed500ee4152384c8423faa6bff2879227a8a
Sven-Hendrik Haase <sh@lutzhaase.com>
2011-05-09 02:06:57 +0200

    Fixing smc init script

--- games/smc/init.sh
+++ games/smc/init.sh
@@ -1,4 +1,4 @@
-install -D -m644 /opt/games/neverball/addons/config.xml /home/gamer/.smc/config.xml
+install -D -m644 /opt/games/smc/addons/config.xml /home/gamer/.smc/config.xml
 xres=$(xrandr -q | grep \* | sed -e "s/^[ \t]*//" -e "s/\([0-9]*x[0-9]*\).*/\1/" | cut -d'x' -f1)
 yres=$(xrandr -q | grep \* | sed -e "s/^[ \t]*//" -e "s/\([0-9]*x[0-9]*\).*/\1/" | cut -d'x' -f2)
 sed -i -e "s/XXXX/$xres/" -e "s/YYYY/$yres/" /home/gamer/.smc/config.xml
